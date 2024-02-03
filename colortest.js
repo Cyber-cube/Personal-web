c03a231717aae200ef3cf2c0fb0197c322f5e5fc
Cyber-cube <112477661+Cyber-cube@users.noreply.github.com>
2024-02-03 12:12:15 +0000

    Change

--- colortest.js
+++ colortest.js
@@ -2,10 +2,16 @@ let redValue = document.getElementById("redSlider");
 let greenValue = document.getElementById("greenSlider");
 let blueValue = document.getElementById("blueSlider");
 
-let redValueText = document.getElementById("redValue");
-let greenValueText = document.getElementById("greenValue");
-let blueValueText = document.getElementById("blueValue");
+redValue.addEventListner("input", redValue())
+greenValue.addEventListner("input", greenValue())
+blueValue.addEventListner("input", blueValue())
 
-redValueText.value = redValue;
-greenValueText.value = greenValue;
-blueValueText.value = blueValue;
+function redValue() {
+  document.getElementById("redValue").textContent = redValue.value;
+}
+function greenValue() {
+  document.getElementById("greenValue").textContent = greenValue.value;
+}
+function blueValue() {
+  document.getElementById("bluedValue").textContent = blueValue.value;
+}
